213e4dde691b632e804424d9d24c2df6718bbecf
Firas Wehbe <firaswehbe@users.noreply.github.com>
2017-06-23 14:08:33 -0500

    cosmetic changes to labels and title and resolution change

--- radar_make16plot_arrangement2.R
+++ radar_make16plot_arrangement2.R
@@ -1,4 +1,4 @@
-# Arrangement: all 16, original order, includes other
+# Arrangement: all 16, custom order, includes other
 # Library
 library(fmsb)
 library(RColorBrewer)
@@ -13,11 +13,11 @@ mycolnames=c(
   "Program\nEvaluation" ,
   "Grants\nand Contracts",
   "Clinical Trial\nManagement Systems",
-  "Electronic Data Capture",
+  "Electronic\nData Capture",
   "Bio-banking\nMgmt",
   "Data Repositories\nincl EHR",
-  "EMR for Research",
-  "Communication",
+  "EMR for\nResearch",
+  "Research Collaboration\nand Communication",
   "Extra Mural\nData Collaboration",
   "Security",
   "IT\nInfrastructure",
@@ -34,7 +34,7 @@ mydata=rbind(rep(50,16) , rep(0,16) , mydata)
 CombinedPallette = brewer.pal(8,"Dark2")
 
 # radarChart Combined Locus
-png(file=paste0("output/radar_loc_combined_16cats_2.png"),width=1800,height=1500,res=150)
+png(file=paste0("output/radar_loc_combined_16cats_2.png"),width=2400,height=2000,res=150)
 radarchart( mydata[c(1,2,3,4,5,6),]  , axistype=1 ,
             #custom line
             plwd=4 , pcol=CombinedPallette[1:4], plty=1, 
@@ -43,14 +43,14 @@ radarchart( mydata[c(1,2,3,4,5,6),]  , axistype=1 ,
             #custom labels
             vlcex=1.2
 )
-title(main = "Locus of Control", cex.main = 2)
+title(main = "Locus of Control (Custom Order)", cex.main = 2)
 legend("bottomleft", legend = c("Informatics","Research Office","CIO/IT","Other"), 
        col = CombinedPallette[1:4],
-       lty = 1, lwd = 4, cex = 1.0)
+       lty = 1, lwd = 4, cex = 1.4)
 dev.off()
 
 # radarChart Combined Sustainability
-png(file=paste0("output/radar_sust_combined_16cats_2.png"),width=1800,height=1500,res=150)
+png(file=paste0("output/radar_sust_combined_16cats_2.png"),width=2400,height=2000,res=150)
 radarchart( mydata[c(1,2,7,8,9,10),]  , axistype=1 ,
             #custom line
             plwd=4 , pcol=CombinedPallette[5:8], plty=1, 
@@ -59,9 +59,9 @@ radarchart( mydata[c(1,2,7,8,9,10),]  , axistype=1 ,
             #custom labels
             vlcex=1.2
 )
-title(main = "Sustainability Model", cex.main = 2)
+title(main = "Sustainability Model (Custom Order)", cex.main = 2)
 legend("bottomleft", legend = c("Institutional","Fee for Service / Charge Back","Grants","Other"), 
        col = CombinedPallette[5:8],
-       lty = 1, lwd = 4, cex = 1.0)
+       lty = 1, lwd = 4, cex = 1.4)
 dev.off()
 
