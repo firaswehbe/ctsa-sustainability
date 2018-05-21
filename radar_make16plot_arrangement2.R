5a8ef5129215eeccb8646a2d6b23061e4abaeade
Firas Wehbe <firaswehbe@users.noreply.github.com>
2018-05-21 14:10:25 -0500

    Tweaks for paper graphics by Jihad

--- radar_make16plot_arrangement2.R
+++ radar_make16plot_arrangement2.R
@@ -8,6 +8,24 @@ colorder = c(1,4,3,7,5,2,15,10,6,8,11,16,9,12,13,14)
 mydata=myanswersumsdf[,colorder]
 # Rename columns below to tweak if you want
 mycolnames=c(
+  "Regulatory\nSystems",
+  "Service Request",
+  "Program\nEvaluation",
+  "Grants and\nContracts",
+  "CTMS",
+  "Electronic\nData Capture",
+  "Biobanks",
+  "Data Repositories\nincluding EHR ",
+  "EHR-Research\nInterface",
+  "Communication",
+  "Extramural Data\nCollaborations",
+  "Security",
+  "IT Infrastructure",
+  "Governance",
+  "Training & Support",
+  "Informatics\nResearch"
+)
+longcolnames=c(
   "Regulatory Compliance\nfor Research" ,
   "Service Request\nand Fulfillment" ,
   "Program\nEvaluation" ,
@@ -34,33 +52,35 @@ mydata=rbind(rep(50,16) , rep(0,16) , mydata)
 CombinedPallette = brewer.pal(8,"Dark2")
 
 # radarChart Combined Locus
-png(file=paste0("output/radar_loc_combined_16cats_2.png"),width=2400,height=2000,res=150)
+png(file=paste0("output/radar_loc_combined_16cats_2.png"),width=10.2,height=8.5,res=200, units = "in")
 radarchart( mydata[c(1,2,3,4,5,6),]  , axistype=1 ,
             #custom line
             plwd=4 , pcol=CombinedPallette[1:4], plty=1, 
             #custom the grid
-            cglcol="grey", cglty=1, axislabcol="grey", seg=5, caxislabels=seq(0,50,10), cglwd=0.8,
+            cglcol="grey40", cglty=1, axislabcol="grey40", seg=5, caxislabels=seq(0,50,10), cglwd=0.8,
             #custom labels
             vlcex=1.2
 )
-title(main = "Locus of Control (Custom Order)", cex.main = 2)
-legend("bottomleft", legend = c("Informatics","Research Office","CIO/IT","Other"), 
+# title(main = "Locus of Control (Custom Order)", cex.main = 2)
+legend("topleft", legend = c("Informatics","Research Office","CIO/IT","Other"),
+       bty = "n",
        col = CombinedPallette[1:4],
        lty = 1, lwd = 4, cex = 1.4)
 dev.off()
 
 # radarChart Combined Sustainability
-png(file=paste0("output/radar_sust_combined_16cats_2.png"),width=2400,height=2000,res=150)
+png(file=paste0("output/radar_sust_combined_16cats_2.png"),width=10.2,height=8.5,res=200, units = "in")
 radarchart( mydata[c(1,2,7,8,9,10),]  , axistype=1 ,
             #custom line
             plwd=4 , pcol=CombinedPallette[5:8], plty=1, 
             #custom the grid
-            cglcol="grey", cglty=1, axislabcol="grey", seg=5, caxislabels=seq(0,50,10), cglwd=0.8,
+            cglcol="grey40", cglty=1, axislabcol="grey40", seg=5, caxislabels=seq(0,50,10), cglwd=0.8,
             #custom labels
             vlcex=1.2
 )
-title(main = "Sustainability Model (Custom Order)", cex.main = 2)
-legend("bottomleft", legend = c("Institutional","Fee for Service / Charge Back","Grants","Other"), 
+# title(main = "Sustainability Model (Custom Order)", cex.main = 2)
+legend("topleft", legend = c("Institutional","Fee-for-Service","Grants","Other"), 
+       bty = "n",
        col = CombinedPallette[5:8],
        lty = 1, lwd = 4, cex = 1.4)
 dev.off()
